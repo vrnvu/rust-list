9e531340fc4c6f43dfdb35fba183b511116990fc
Arnau Diaz <arnau.diaz@adevinta.com>
2022-03-25 21:38:30 +0100

    peek

--- src/second.rs
+++ src/second.rs
@@ -30,6 +30,14 @@ impl<T> List<T> {
 			},
 		}
 	}
+
+	pub fn peek(&self) -> Option<&T> {
+		self.head.as_ref().map(|node| &node.elem)
+	}
+
+	pub fn peek_mut(&mut self) -> Option<&mut T> {
+		self.head.as_mut().map(|node| &mut node.elem)
+	}
 }
 
 impl <T> Drop for List<T> {
@@ -67,5 +75,30 @@ mod test {
 		assert_eq!(list.pop(), Some(50));
 		assert_eq!(list.pop(), Some(1));
 		assert_eq!(list.pop(), None);
+	}
+
+	#[test]
+	fn peek() {
+		let mut list = List::new();
+		assert_eq!(list.peek(), None);
+		list.push(1); list.push(2); list.push(3);
+
+		assert_eq!(list.peek(), Some(&3));
+		assert_eq!(list.pop(), Some(3));
+	}
+
+	#[test]
+	fn peek_mut() {
+		let mut list = List::new();
+		assert_eq!(list.peek_mut(), None);
+		list.push(1); list.push(2); list.push(3);
+
+		assert_eq!(list.peek_mut(), Some(&mut 3));
+
+		list.peek_mut().map(|value| {
+		*value = 42
+		});
+
+		assert_eq!(list.pop(), Some(42));
 	}
 }

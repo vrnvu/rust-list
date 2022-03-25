066bfd4573a013ff1d0f744b311d1dff359a913d
Arnau Diaz <arnau.diaz@adevinta.com>
2022-03-25 21:45:08 +0100

    iterator

--- src/second.rs
+++ src/second.rs
@@ -7,8 +7,21 @@ struct Node<T> {
     next: Option<Box<Node<T>>>,
 }
 
+pub struct IntoIter<T>(List<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
 
 impl<T> List<T> {
+	pub fn into_iter(self) -> IntoIter<T> {
+		return IntoIter(self)
+	}
+
 	pub fn new() -> Self {
 		List {head: Option::None}
 	}
@@ -100,5 +113,17 @@ mod test {
 		});
 
 		assert_eq!(list.pop(), Some(42));
+	}
+
+	#[test]
+	fn into_iter() {
+	let mut list = List::new();
+	list.push(1); list.push(2); list.push(3);
+
+	let mut iter = list.into_iter();
+	assert_eq!(iter.next(), Some(3));
+	assert_eq!(iter.next(), Some(2));
+	assert_eq!(iter.next(), Some(1));
+	assert_eq!(iter.next(), None);
 	}
 }
